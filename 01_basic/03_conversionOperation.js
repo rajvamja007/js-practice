753df0546b418efc3ce29353c1c8c76f4aed0eb2
rajvamja007 <139110452+rajvamja007@users.noreply.github.com>
2024-07-26 05:46:53 +0000

    conersion operation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,77 @@
+let score = "33abc" // 33 is number and "33" is string type
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // variable typeof is number and score typeof is string
+console.log(valueInNumber); // values is NaN (Not a Number)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true=1, false=0
+
+
+//-------------------------------------
+
+let isLoggedIn = "Raj" // 1=true, 0=false / "" => fasle / "Raj" => true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//------------------------------------ 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); // Number to String Conversion
+
+//----------------------------------Opration----------------------------------
+
+//---------------------------------------
+let value = 3
+let negValue = -value
+console.log(negValue); //convert into Negative value
+
+//-----------------------------------------
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1 = "Hello "
+let str2 = "Friends"
+
+let str3 = str1 + str2
+console.log(str3); // Add to string 
+
+//--------------------------------------
+
+console.log("1"+2);
+console.log(2+"1");
+console.log("1"+2+2);
+console.log(2+2+"1");
+
+console.log((3 + 4) * 5 % 6); // difficulat
+
+//----------------------------------------
+
+console.log(+true);
+console.log(+"");
+
+//----------------------------------------
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1,num2,num3);
+
+//-----------------------------------------
+
+let gameCOunter = 100
+gameCOunter++;
+console.log(gameCOunter);
