d422cc294be377ec5030688c6e55560b0b0d8361
rajvamja007 <139110452+rajvamja007@users.noreply.github.com>
2024-07-26 05:09:54 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; 
+
+//console.log(3+3)
+console.log("Raj")
+
+let name = "Raj"
+let age = 20
+let isLoggedIn = false
+let state = null;
+
+
+//number => 2 to 53
+//bigint  //Used to Bid Number
+//string = ""
+//boolean = true/false
+//null = Object 
+//undefined = undefined type
+//symbols = uniqe
+
+//object
+
+console.log(typeof age);   // which type used     
