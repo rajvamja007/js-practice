5a70854b3525fe31b00b2e20c1fd5491e3be64d0
rajvamja007 <139110452+rajvamja007@users.noreply.github.com>
2024-07-26 06:04:36 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+// console.log(2 == 1); //basic comparison
+
+//-------------------------------------------
+
+// console.log("2" > 1);  // convert string to number in 2
+// console.log(1 > "2");
+
+//---------------------------------------------
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);  // null is empty value than 0 is big
+
+//---------------------------------------------
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0); // undefined is not a enter the value than 0 is big
+
+//--------------------------------------------
+
+console.log("2" == 2);
