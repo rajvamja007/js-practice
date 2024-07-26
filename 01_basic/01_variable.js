baf846bcc71671ef67eb644fe0875e386cafd3e9
rajvamja007 <139110452+rajvamja007@users.noreply.github.com>
2024-07-26 05:07:42 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "ABC@gmail.com"
+var accountPassword = "11111"
+accountCity = "Rajkot"
+
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// accountId = 00000 // not allowed in const keyword
+accountEmail = "xyz@gmail.com"
+accountPassword = "22222"
+accountCity = "Amreli"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
